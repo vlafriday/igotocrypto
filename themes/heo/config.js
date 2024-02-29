88254b85604b34631d601368121f02b80520f910
vlafriday <78411322+vlafriday@users.noreply.github.com>
2024-02-29 12:03:44 +0300

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2019-10-06', // Дата создания веб-сайта. Используется для расчета количества дней, в течение которых веб-сайт работает.
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: 'Добро пожаловать в мой блог', url: 'https://vlafriday.space' },
+    { title: 'Добро пожаловать в мой блог', url: 'https://vlafriday.space' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,17 +15,17 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: 'Поделиться программированием',
+  HEO_HERO_TITLE_2: 'и мышление познания',
+  HEO_HERO_TITLE_3: 'vlafriday.space',
+  HEO_HERO_TITLE_4: 'Новая версия онлайн',
+  HEO_HERO_TITLE_5: 'NotionNext4.0 легко настраивать темы',
+  HEO_HERO_TITLE_LINK: 'https://vlafriday.space',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: 'Обязательно посмотри подборки', url: '/tag/search' },
+  HEO_HERO_CATEGORY_2: { title: 'популярные статьи', url: '/tag/carefully' },
+  HEO_HERO_CATEGORY_3: { title: 'практическое руководство', url: '/tag/links' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -34,15 +34,15 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    'Привет！Я',
+    '🔍 Делимся и помогаем с энтузиазмом',
+    '🤝 Специализируемся на взаимодействии и дизайне',
+    '🏃 практичный активист',
+    '🏠 Эксперт по умному дому',
+    '🤖️ Энтузиаст цифровых технологий',
+    '🧱 Механизм группы команд'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://github.com/vlafriday/NotionNext', // Ссылка на кнопку внизу профиля
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
@@ -105,10 +105,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: 'канал связи',
+  HEO_SOCIAL_CARD_TITLE_2: 'Присоединяйтесь к нашему сообществу, чтобы обсуждать и делиться',
+  HEO_SOCIAL_CARD_TITLE_3: 'Нажмите, чтобы присоединиться к сообществу',
+  HEO_SOCIAL_CARD_URL: 'https://vlafriday.space',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
@@ -127,7 +127,7 @@ const CONFIG = {
   HEO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
   HEO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  HEO_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
+  HEO_ARTICLE_COPYRIGHT: false, // 显示文章版权声明
   HEO_ARTICLE_RECOMMEND: true, // 文章关联推荐
 
   HEO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
