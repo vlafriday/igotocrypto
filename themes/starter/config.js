17578d3cfe43427cb6b13ebd7f91285be0e937fc
vpyatnitsa <pakiboy@yandex.ru>
2024-03-01 09:58:11 +0300

    русификация темы стартер

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -83,7 +83,7 @@ const CONFIG = {
   STARTER_PRICING_2_PRICE_PERIOD: 'каждый месяц',
   STARTER_PRICING_2_HEADER: 'функциональная точка',
   STARTER_PRICING_2_FEATURES: 'Включает в себя стартовую версию, исходный код проекта, внутреннее сообщество, технические консультации, SEO-оптимизацию.', // разделенные запятыми
-  STARTER_PRICING_2_BUTTON_TEXT: '立即购买',
+  STARTER_PRICING_2_BUTTON_TEXT: 'Купить сейчас',
   STARTER_PRICING_2_BUTTON_URL: 'https://tangly1024.lemonsqueezy.com/checkout/buy/590ad70a-c3b7-4caf-94ec-9ca27bde06d4',
 
   STARTER_PRICING_3_TITLE: 'премиум-версия',
@@ -103,75 +103,76 @@ const CONFIG = {
   //CONFIG и переменные окружения здесь не поддерживаются, вам необходимо изменить код по очереди.
   STARTER_TESTIMONIALS_ITEMS: [
     {
-      STARTER_TESTIMONIALS_ITEM_TEXT: '感谢大佬的方法。之前尝试过Super、Potion等国外的第三方平台，实现效果一般，个性化程度远不如这个方法，已经用起来了！ ',
+      STARTER_TESTIMONIALS_ITEM_TEXT: 'Спасибо за метод старшего брата. Я уже пробовал Super, Potion и другие зарубежные сторонние платформы, и их реализация была средней и гораздо менее персонализированной, чем этот метод, который я уже использовал! ',
       STARTER_TESTIMONIALS_ITEM_AVATAR: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F22de3fcb-d90d-4271-bc01-f815f476122b%2F4FE0A0C0-E487-4C74-BF8E-6F01A27461B8-14186-000008094BC289A6.jpg?table=collection&id=a320a2cc-6ebe-4a8d-95cc-ea94e63bced9&width=200',
       STARTER_TESTIMONIALS_ITEM_NICKNAME: 'Ryan_G',
-      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'Ryan`Log 站长',
+      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'Ryan`Log начальник станции',
       STARTER_TESTIMONIALS_ITEM_URL: 'https://blog.gaoran.xyz/'
     },
     {
-      STARTER_TESTIMONIALS_ITEM_TEXT: '很喜欢这个主题，本代码小白用三天台风假期搭建出来了，还根据大佬的教程弄了自定义域名，十分感谢，已请喝咖啡~',
+      STARTER_TESTIMONIALS_ITEM_TEXT: 'Очень нравится эта тема, этот код белый с тремя днями тайфун праздник построить из, но и в соответствии с учебниками старшего брата, чтобы получить пользовательское доменное имя, большое спасибо, пригласил пить кофе ~!',
       STARTER_TESTIMONIALS_ITEM_AVATAR: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0d33d169-f932-41ff-ac6b-88a923c08e02%2F%25E5%25A4%25B4%25E5%2583%258F.jfif?table=collection&id=7787658d-d5c0-4f34-8e32-60c523dfaba3&width=400',
       STARTER_TESTIMONIALS_ITEM_NICKNAME: 'Asenkits',
-      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: '阿森的百宝袋 站长',
+      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'Веб-мастер "Мешок сокровищ Сэма',
       STARTER_TESTIMONIALS_ITEM_URL: 'https://asenkits.top/'
     },
     {
-      STARTER_TESTIMONIALS_ITEM_TEXT: '呜呜呜，经过一个下午的努力，终于把博客部署好啦，非常感谢Tangly1024大佬的框架和教程，这是我有生之年用过的最好用的博客框架┭┮﹏┭┮。从今之后，我就可以在自己的博客里bb啦，( •̀ ω •́ )y ',
+      STARTER_TESTIMONIALS_ITEM_TEXT: 'Уууу, после полудня напряженной работы я наконец развернул блог. Я очень благодарен Tangly1024 за его структуру и учебные пособия. Это лучшая платформа для ведения блога, которую я когда-либо использовал в своей жизни┭┮﹏┭┮. С этого момента я могу bb в своем блоге, ( •̀ ω •́ )y
+',
       STARTER_TESTIMONIALS_ITEM_AVATAR: 'https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F6c096b44-beb9-48ee-8f92-1efdde47f3a3%2F338962f1-d352-49c7-9a1b-746e35a7005c%2Fhf.png?table=block&id=ce5a48a9-d77a-4843-a3d9-a78cd4f794ce&spaceId=6c096b44-beb9-48ee-8f92-1efdde47f3a3&width=100&userId=27074aef-7216-41ed-baef-d9b53addd870&cache=v2',
       STARTER_TESTIMONIALS_ITEM_NICKNAME: 'DWIND',
-      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: '且听风吟 站长',
+      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: '  Слушайте ветер и прислушивайтесь к его шуму.  ',
       STARTER_TESTIMONIALS_ITEM_URL: 'https://www.dwind.top/'
     },
     {
-      STARTER_TESTIMONIALS_ITEM_TEXT: '感谢提供这么好的项目哈哈 之前一直不知道怎么部署(别的项目好难好复杂)这个相对非常简单 新手非常友好哦',
+      STARTER_TESTIMONIALS_ITEM_TEXT: 'Спасибо за предоставление такого хорошего проекта, ха-ха. Я никогда раньше не знал, как его развернуть (другие проекты сложны и сложны). Этот относительно прост и очень удобен для новичков.',
       STARTER_TESTIMONIALS_ITEM_AVATAR: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd52f6766-3e32-4c3d-8529-46e1f214360f%2Ffavicon.svg?table=collection&id=7d76aad5-a2c4-4d9a-887c-c7913fae4eed&width=400',
-      STARTER_TESTIMONIALS_ITEM_NICKNAME: '迪升disheng ',
-      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'AI资源分享 Blog',
+      STARTER_TESTIMONIALS_ITEM_NICKNAME: 'Дишенг ',
+      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'Совместное использование ресурсов ИИ Blog',
       STARTER_TESTIMONIALS_ITEM_URL: 'https://blog.disheng.org/'
     },
     {
-      STARTER_TESTIMONIALS_ITEM_TEXT: '灰常感谢大佬的博客项目，能将博客和notion结合起来，这一直是我挺期待的博客模式。',
+      STARTER_TESTIMONIALS_ITEM_TEXT: 'Хуэй очень благодарен своему боссу за его проект блога, который может сочетать ведение блога и уведомление. Я всегда с нетерпением ждал этой модели ведения блога.',
       STARTER_TESTIMONIALS_ITEM_AVATAR: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fafb21381-f51b-4fd0-9998-800dbeb64dbe%2Favatar.png?table=block&id=195935d2-0d8d-49fc-bd81-1db42ee50840&spaceId=6c096b44-beb9-48ee-8f92-1efdde47f3a3&width=100&userId=27074aef-7216-41ed-baef-d9b53addd870&cache=v2',
       STARTER_TESTIMONIALS_ITEM_NICKNAME: 'AnJhon',
-      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'Anjhon`s Blog 站长',
+      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'Anjhon`s Blog веб-мастер',
       STARTER_TESTIMONIALS_ITEM_URL: 'https://www.anjhon.top'
     },
     {
-      STARTER_TESTIMONIALS_ITEM_TEXT: '用好久了，太感谢了',
+      STARTER_TESTIMONIALS_ITEM_TEXT: 'Давно пользуюсь, спасибо большое',
       STARTER_TESTIMONIALS_ITEM_AVATAR: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fe4f391d7-7d65-4c05-a82c-c6e2c40f06e4%2Fa2a7641a26b367608c6ef28ce9b7e983_(2).png?table=block&id=a386eb0e-4c07-4b18-9ece-bba4e79ce21c&spaceId=6c096b44-beb9-48ee-8f92-1efdde47f3a3&width=100&userId=27074aef-7216-41ed-baef-d9b53addd870&cache=v2',
       STARTER_TESTIMONIALS_ITEM_NICKNAME: 'LUCEN',
-      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'LUCEN考验辅导 站长',
+      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'Веб-мастер LUCEN по консультированию по тестированию',
       STARTER_TESTIMONIALS_ITEM_URL: 'https://www.lucenczz.top/'
     }
   ],
 
-  //   FAQ模块
-  STARTER_FAQ_TITLE: '常见问题解答',
-  STARTER_FAQ_TEXT_1: '有任何问题吗？请看这里',
-  STARTER_FAQ_TEXT_2: '我们收集了常见的用户疑问',
+  //   Модуль часто задаваемых вопросов
+  STARTER_FAQ_TITLE: 'Часто задаваемые вопросы',
+  STARTER_FAQ_TEXT_1: 'Есть вопросы? Пожалуйста, посмотрите здесь',
+  STARTER_FAQ_TEXT_2: 'Мы собрали распространенные вопросы пользователей',
 
-  STARTER_FAQ_1_QUESTION: 'NotionNext有帮助文档吗？',
-  STARTER_FAQ_1_ANSWER: 'NotionNext提供了<a href="https://docs.tangly1024.com/about" className="underline">帮助文档</a>，操作<a href="https://www.bilibili.com/video/BV1fM4y1L7Qi/" className="underline">演示视频</a>，以及<a href="https://docs.tangly1024.com/article/chat-community" className="underline">交流社群</a>来协助您完成网站的搭建部署',
+  STARTER_FAQ_1_QUESTION: 'Есть ли полезная документация для NotionNext?',
+  STARTER_FAQ_1_ANSWER: 'NotionNext предоставляет <a href="https://docs.tangly1024.com/about" className="underline">Справочную документацию</a> и управляет <a href="https://www.bilibili.com/video". /BV1fM4y1L7Qi/" className="underline">Демо-видео</a> и <a href="https://docs.tangly1024.com/article/chat-community" className="underline">Сообщество общения</a > а>Чтобы помочь вам в завершении создания и развертывания веб-сайта',
 
-  STARTER_FAQ_2_QUESTION: '部署后要如何编写文章？',
-  STARTER_FAQ_2_ANSWER: '您可以在Notion中之间添加或修改类型为Post的页面，内容将被实时同步在站点中，详情参考<a className="underline" href="https://docs.tangly1024.com/article/start-to-write">《帮助文档》</a>',
+  STARTER_FAQ_2_QUESTION: 'Как писать статьи после развертывания?',
+  STARTER_FAQ_2_ANSWER: 'Вы можете добавлять или изменять страницы типа Публикация в Notion, и контент будет синхронизироваться на сайте в режиме реального времени. Подробную информацию см. на странице <a className="underline" href="https://docs.tangly1024.com". /article/start -to-write">"Справочная документация"</a>',
 
-  STARTER_FAQ_3_QUESTION: '站点部署失败，更新失败？',
-  STARTER_FAQ_3_ANSWER: '通常是配置修改错误导致，请检查配置或者重试操作步骤，或者通过Vercel后台的Deployments中找到错误日志，并向网友求助',
+  STARTER_FAQ_3_QUESTION: 'Не удалось развернуть сайт, не удалось обновить?',
+  STARTER_FAQ_3_ANSWER: 'Обычно это вызвано ошибками изменения конфигурации. Проверьте конфигурацию или повторите шаги операции или найдите журнал ошибок в разделе «Развертывания» в фоновом режиме Vercel и обратитесь за помощью к пользователям сети.',
 
-  STARTER_FAQ_4_QUESTION: '文章没有实时同步？',
-  STARTER_FAQ_4_ANSWER: '先检查Notion_Page_ID是否正确配置，其次由于博客的每个页面都有独立缓存，刷新网页后即可解决',
+  STARTER_FAQ_4_QUESTION: 'Статьи не синхронизируются в реальном времени?',
+  STARTER_FAQ_4_ANSWER: 'Сначала проверьте, правильно ли настроен Notion_Page_ID.Во-вторых, поскольку каждая страница блога имеет независимый кеш, решить эту проблему можно после обновления веб-страницы.',
 
-  STARTER_TEAM_TITLE: '团队成员',
-  STARTER_TEAM_TEXT_1: '我们的开发者团队',
-  STARTER_TEAM_TEXT_2: 'NotionNext 由众多开源技术爱好者们共同合作完成，感谢每一位<a className="underline" href="https://github.com/tangly1024/NotionNext/graphs/contributors">贡献者</a>',
+  STARTER_TEAM_TITLE: 'Участники команды',
+  STARTER_TEAM_TEXT_1: 'Наша команда разработчиков',
+  STARTER_TEAM_TEXT_2: 'NotionNext– это результат сотрудничества многих энтузиастов технологий с открытым исходным кодом. Благодарим каждого <a className="underline" href="https://github.com/tangly1024/NotionNext/graphs/contributors">участника</a>.',
 
-  // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
+  // Переменные CONFIG и среды здесь не поддерживаются, и код здесь необходимо изменять поочередно.
   STARTER_TEAM_ITEMS: [
     {
       STARTER_TEAM_ITEM_AVATAR: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fa06c61bb-980e-4180-bc18-c15f92c78bb4%2Ftangly1024.jpg?table=collection&id=8e7acf17-de09-4fa1-abde-b5b80ad4a813&t=8e7acf17-de09-4fa1-abde-b5b80ad4a813&width=100&cache=v2',
-      STARTER_TEAM_ITEM_NICKNAME: 'Tangly',
+      STARTER_TEAM_ITEM_NICKNAME: 'vlafriday',
       STARTER_TEAM_ITEM_DESCRIPTION: 'Developer'
     },
     {
@@ -191,23 +192,23 @@ const CONFIG = {
     }
   ],
 
-  // 博客文章区块
-  STARTER_BLOG_TITLE: '我们的博客',
-  STARTER_BLOG_COUNT: 3, // 首页博文区块默认展示前3篇文章
-  STARTER_BLOG_TEXT_1: '最近的新闻',
-  STARTER_BLOG_TEXT_2: '这里会发布一些关于NotionNext的最新动态，包括新的动向、新的未来计划，以及新功能的特性',
+  // Блок постов в блоге
+  STARTER_BLOG_TITLE: 'Наш блог',
+  STARTER_BLOG_COUNT: 3, // Блок сообщений блога на главной странице по умолчанию отображает первые 3 статьи.
+  STARTER_BLOG_TEXT_1: 'Последние новости',
+  STARTER_BLOG_TEXT_2: 'Здесь мы будем публиковать некоторые последние новости о NotionNext, включая новые тенденции, новые планы на будущее и новые функции.',
 
-  // 联系模块
-  STARTER_CONTACT_TITLE: '联系我们',
-  STARTER_CONTACT_TEXT: '告诉我们您遇到的问题',
-  STARTER_CONTACT_LOCATION_TITLE: '我们的位置',
-  STARTER_CONTACT_LOCATION_TEXT: '中国，福建',
-  STARTER_CONTACT_EMAIL_TITLE: '我们如何帮助您？',
-  STARTER_CONTACT_EMAIL_TEXT: 'mail@tangly1024.com',
+  // контактный модуль
+  STARTER_CONTACT_TITLE: 'связаться с нами',
+  STARTER_CONTACT_TEXT: 'Расскажите нам о проблеме, с которой вы столкнулись',
+  STARTER_CONTACT_LOCATION_TITLE: 'наша позиция',
+  STARTER_CONTACT_LOCATION_TEXT: 'Фуцзянь Китай',
+  STARTER_CONTACT_EMAIL_TITLE: 'Как мы можем вам помочь?',
+  STARTER_CONTACT_EMAIL_TEXT: 'mail@vlafriday.space',
 
-  // 嵌入外部表单
-  STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/yfctc7', // 基于NoteForm创建，将留言数据存在Notion中
-  //   自定义留言表单
+  // Встроить внешнюю форму
+  STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/yfctc7', // Создан на основе NoteForm и хранит данные сообщений в Notion.
+  //   Пользовательская форма сообщения
   //   STARTER_CONTACT_MSG_TITLE: '向我们留言',
   //   STARTER_CONTACT_MSG_NAME: '姓名',
   //   STARTER_CONTACT_MSG_EMAIL: '邮箱地址',
@@ -216,7 +217,7 @@ const CONFIG = {
   //   STARTER_CONTACT_MSG_SEND: '发送消息',
   //   STARTER_CONTACT_MSG_THANKS: '感谢您的留言',
 
-  // 合作伙伴的图标
+  // Значок партнера
   STARTER_BRANDS: [
     {
       IMAGE: '/images/starter/brands/graygrids.svg',
@@ -250,55 +251,55 @@ const CONFIG = {
     }
   ],
 
-  STARTER_FOOTER_SLOGAN: '我们通过技术为品牌和公司创造数字体验。',
+  STARTER_FOOTER_SLOGAN: 'Мы создаем цифровой опыт для брендов и компаний с помощью технологий.',
 
-  // 页脚三列菜单组
+  // Группа меню в трех столбцах нижнего колонтитула
   STARTER_FOOTER_LINK_GROUP: [
     {
-      TITLE: '关于我们',
+      TITLE: 'о нас',
       LINK_GROUP: [
-        { TITLE: '官方主页', URL: '/#home' },
-        { TITLE: '操作文档', URL: 'https://docs.tangly1024.com/about' },
-        { TITLE: '帮助支持', URL: 'https://docs.tangly1024.com/article/how-to-question' },
-        { TITLE: '合作申请', URL: 'https://docs.tangly1024.com/my-service' }
+        { TITLE: 'Официальная домашняя страница', URL: '/#home' },
+        { TITLE: 'Эксплуатационная документация', URL: 'https://docs.tangly1024.com/about' },
+        { TITLE: 'Помощь поддержка', URL: 'https://docs.tangly1024.com/article/how-to-question' },
+        { TITLE: 'Заявка на сотрудничество', URL: 'https://docs.tangly1024.com/my-service' }
       ]
     },
     {
-      TITLE: '功能特性',
+      TITLE: 'Функции',
       LINK_GROUP: [
-        { TITLE: '部署指南', URL: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next' },
-        { TITLE: '升级指南', URL: 'https://docs.tangly1024.com/article/how-to-update-notionnext' },
-        { TITLE: '最新版本', URL: 'https://docs.tangly1024.com/article/latest' }
+        { TITLE: 'Руководство по развертыванию', URL: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next' },
+        { TITLE: 'Руководство по обновлению', URL: 'https://docs.tangly1024.com/article/how-to-update-notionnext' },
+        { TITLE: 'Последняя версия', URL: 'https://docs.tangly1024.com/article/latest' }
       ]
     },
     {
-      TITLE: 'Notion写作',
+      TITLE: 'NotionNext',
       LINK_GROUP: [
-        { TITLE: 'Notion开始写作', URL: 'https://docs.tangly1024.com/article/start-to-write' },
-        { TITLE: '快捷键提升效率', URL: 'https://docs.tangly1024.com/article/notion-short-key' },
-        { TITLE: '中国大陆使用Notion', URL: 'https://docs.tangly1024.com/article/notion-faster' }
+        { TITLE: 'Notion начни писать', URL: 'https://docs.tangly1024.com/article/start-to-write' },
+        { TITLE: 'Сочетания клавиш повышают эффективность', URL: 'https://docs.tangly1024.com/article/notion-short-key' },
+        { TITLE: 'Материковый Китай использует Notion', URL: 'https://docs.tangly1024.com/article/notion-faster' }
       ]
     }
   ],
 
-  STARTER_FOOTER_BLOG_LATEST_TITLE: '最新文章',
+  STARTER_FOOTER_BLOG_LATEST_TITLE: 'Последние статьи',
 
-  STARTER_FOOTER_PRIVACY_POLICY_TEXT: '隐私政策',
+  STARTER_FOOTER_PRIVACY_POLICY_TEXT: 'политика конфиденциальности',
   STARTER_FOOTER_PRIVACY_POLICY_URL: '/privacy-policy',
 
-  STARTER_FOOTER_PRIVACY_LEGAL_NOTICE_TEXT: '法律声明',
+  STARTER_FOOTER_PRIVACY_LEGAL_NOTICE_TEXT: 'Юридические уведомления',
   STARTER_FOOTER_PRIVACY_LEGAL_NOTICE_URL: '/legacy-notice',
 
-  STARTER_FOOTER_PRIVACY_TERMS_OF_SERVICE_TEXT: '服务协议',
+  STARTER_FOOTER_PRIVACY_TERMS_OF_SERVICE_TEXT: 'Сервисное соглашение',
   STARTER_FOOTER_PRIVACY_TERMS_OF_SERVICE_URL: '/terms-of-use',
 
-  // 404页面的提示语
-  STARTER_404_TITLE: '我们似乎找不到您要找的页面。',
-  STARTER_404_TEXT: '抱歉！您要查找的页面不存在。可能已经移动或删除。',
-  STARTER_404_BACK: '回到主页',
+  // Подсказки на странице 404
+  STARTER_404_TITLE: 'Кажется, мы не можем найти страницу, которую вы ищете。',
+  STARTER_404_TEXT: 'Пожалейте! Страница, которую вы ищете, не существует. Возможно, он был перемещен или удален.',
+  STARTER_404_BACK: 'Вернуться на главную страницу',
 
-  STARTER_POST_REDIRECT_ENABLE: true, // 默認開啟重定向
-  STARTER_POST_REDIRECT_URL: 'https://blog.tangly1024.com', // 重定向域名
+  STARTER_POST_REDIRECT_ENABLE: true, // Перенаправление включено по умолчанию
+  STARTER_POST_REDIRECT_URL: 'https://blog.tangly1024.com', // Перенаправить доменное имя
   STARTER_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_STARTER_NEWSLETTER || false // 是否开启邮件订阅 请先配置mailchimp功能 https://docs.tangly1024.com/article/notion-next-mailchimp
 }
 export default CONFIG
