24855dbfe1356559bb61ed6e63fd7de054b46e7c
vlafriday <78411322+vlafriday@users.noreply.github.com>
2024-03-04 10:38:30 +0300

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -23,7 +23,7 @@ const BLOG = {
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'vlafriday, блог, Владислав Пятница, Тот самый Фрайдей', // Ключевые слова сайта, разделенные английскими запятыми
 
   // Социальная ссылка. Если она не требуется, оставьте ее пустой, например CONTACT_WEIBO:''
-  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
+  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // Адрес электронной почты mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || 'https://twitter.com/vlafriday', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
