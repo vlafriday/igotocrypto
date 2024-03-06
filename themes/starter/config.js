82055f11d94a48a3ce0bd91d93769dc4bf6f8a2b
vpyatnitsa <pakiboy@yandex.ru>
2024-03-06 16:15:20 +0300

    изменения в конфиге стартер

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -3,21 +3,24 @@
  */
 const CONFIG = {
 
-  // По умолчанию отображается только текст логотипа, если задано изображение логотипа, то слева от текста будет отображаться иконка.
-  STARTER_LOGO: '', // Пример обычного изображения логотипа：/images/starter/logo/logo.svg
-  STARTER_LOGO_WHITE: '', // Пример светлого логотипа на прозрачном фоне： /images/starter/logo/logo-white.svg
+  // 默认只展示Logo文字，如果设置了logo图片，会在文字左侧显示图标
+  STARTER_LOGO: '', // 普通logo图片 示例：/images/starter/logo/logo.svg
+  STARTER_LOGO_WHITE: '', // 透明底浅色logo 示例： /images/starter/logo/logo-white.svg
 
   // MENU ， 菜单部分不在此处配置，请在Notion数据库中添加MENU
 
   // 英雄区块导航
-  STARTER_HERO_TITLE_1: 'Бесплатный конструктор сайтов с открытым исходным кодом, основанный на Notion notes', // Зона Героев текст
-  STARTER_HERO_TITLE_2: 'Умножьте свою ценность, создав сайт без восприятия через заметки', // Зона Героев текст
-  STARTER_HERO_TITLE_3: 'Создан по последнему слову техники', // Зона Героев текст
-  STARTER_HERO_BUTTON_1_TEXT: 'Мой путь', // Кнопка зоны героя
-  STARTER_HERO_BUTTON_1_URL: 'https://www.vlafriday.space/category/%D0%92%D0%B8%D0%B4%D0%B5%D0%BE', // Кнопка зоны героя
-  STARTER_HERO_BUTTON_2_TEXT: 'Youtube канал', // Кнопка зоны героя
-  STARTER_HERO_BUTTON_2_URL: 'https://youtube.com/@vlafriday', // Кнопка зоны героя
-  STARTER_HERO_PREVIEW_IMAGE: 'https://file.notion.so/f/f/552435b8-f15d-41bf-b8a2-713cf6d238c5/dc1c0db2-adaf-4efa-a228-e47c062029d8/G3o8RUTBg2o.jpg?id=2d739e23-0da8-43b7-a58f-e51939645a5d&table=block&spaceId=552435b8-f15d-41bf-b8a2-713cf6d238c5&expirationTimestamp=1709445600000&signature=EabaRgYKNAs_rHGqUPJZaECjuwsMFDbrk48gZkhXUWI&downloadName=G3o8RUTBg2o.jpg', // Изображение предварительного просмотра продукта, по умолчанию читается публичная директория изображения
+  STARTER_HERO_ENABLE: true, // 开启英雄区
+  STARTER_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
+  STARTER_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
+  // 英雄区两个按钮，如果TEXT留空则隐藏按钮
+  STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
+  STARTER_HERO_BUTTON_1_URL: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
+
+  STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
 
   // Верхняя правая навигация
   STARTER_NAV_BUTTON_1_TEXT: 'Войти',
