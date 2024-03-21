675f51be8cf3e44e6c5abe4dcd39f98eccc67db3
vlafriday <pakiboy@yandex.ru>
2024-03-21 21:43:21 +0300

    config landind

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -7,15 +7,15 @@ const CONFIG = {
   LANDING_HEADER_BUTTON_2_URL: 'https://blog.tangly1024.com/',
 
   // 首页大图英雄板块
-  LANDING_HERO_TITLE_1: 'NotionNext',
-  LANDING_HERO_P_1: '快速搭建独立站、轻松放大品牌价值！',
+  LANDING_HERO_TITLE_1: 'Идущий к крипте',
+  LANDING_HERO_P_1: 'Начни зарабатывать в крипте',
   LANDING_HERO_BUTTON_1_TEXT: '开始体验',
   LANDING_HERO_BUTTON_1_LINK: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next',
   LANDING_HERO_BUTTON_2_TEXT: 'Узнать больше',
   LANDING_HERO_BUTTON_2_LINK: 'https://docs.tangly1024.com/about',
   LANDING_HERO_VIDEO_IMAGE: 'https://images.unsplash.com/photo-1634704784915-aacf363b021f?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=4800',
   //   HERO_VIDEO_URL: '/videos/video.mp4',
-  LANDING_HERO_VIDEO_IFRAME: 'https://www.youtube.com/watch?v=D8aGQW7ynsg', ////player.bilibili.com/player.html?aid=913088616&bvid=BV1fM4y1L7Qi&cid=1187218697&page=1&&high_quality=1
+  LANDING_HERO_VIDEO_IFRAME: 'https://www.youtube.com/embed/D8aGQW7ynsg?si=yo0oRgJ9v8cn-GcA', ////player.bilibili.com/player.html?aid=913088616&bvid=BV1fM4y1L7Qi&cid=1187218697&page=1&&high_quality=1
   LANDING_HERO_VIDEO_TIPS: 'Watch the full video (2 min)',
 
   // 特性介绍
