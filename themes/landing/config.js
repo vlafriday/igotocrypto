462e438cedecde8f01f1f80d23f1d445a4bc3d4f
vlafriday <pakiboy@yandex.ru>
2024-03-22 06:10:19 +0300

    config landing

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -51,36 +51,36 @@ const CONFIG = {
   LANDING_TESTIMONIALS_P: 'Сайт охватывает широкий спектр отраслей, включая недвижимость, образование, архитектуру, медицину, машиностроение, IT, электронику, программное обеспечение, собственные СМИ, цифровых кочевников, короткие видеоролики, электронную коммерцию, студентов, любителей фотографии, путешествий и так далее.',
 
   LANDING_TESTIMONIALS_AVATAR: 'https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F552435b8-f15d-41bf-b8a2-713cf6d238c5%2F768328da-b823-4c95-a7ae-c9c8ea399109%2Favatar.png?table=collection&id=6759dcbc-020c-4e68-a98f-b8d5c898467e&t=6759dcbc-020c-4e68-a98f-b8d5c898467e&width=800&cache=v2',
-  LANDING_TESTIMONIALS_NICKNAME: 'Ryan_G',
-  LANDING_TESTIMONIALS_ID: 'Ryan`Log 站长',
-  LANDING_TESTIMONIALS_SOCIAL_NAME: '@Gaoran',
-  LANDING_TESTIMONIALS_SOCIAL_URL: 'https://blog.gaoran.xyz/',
-  LANDING_TESTIMONIALS_WORD: '“ 感谢大佬的方法。之前尝试过Super、Potion等国外的第三方平台，实现效果一般，个性化程度远不如这个方法，已经用起来了！ “',
+  LANDING_TESTIMONIALS_NICKNAME: 'Тот Самый Фрайдей',
+  LANDING_TESTIMONIALS_ID: 'Тот самый Фрайдей',
+  LANDING_TESTIMONIALS_SOCIAL_NAME: '@vlafriday',
+  LANDING_TESTIMONIALS_SOCIAL_URL: 'https://vlafriday.space',
+  LANDING_TESTIMONIALS_WORD: 'Спасибо за метод старшего брата. Я уже пробовал Super, Potion и другие зарубежные сторонние платформы, и реализация была средней и гораздо менее персонализированной, чем этот метод, который я уже использовал!',
 
   LANDING_POST_REDIRECT_ENABLE: process.env.NEXT_PUBLIC_POST_REDIRECT_ENABLE || false, // 是否开启文章地址重定向 ； 用于迁移旧网站域名
   LANDING_POST_REDIRECT_URL: process.env.NEXT_PUBLIC_POST_REDIRECT_URL || 'https://blog.tangly1024.com', // 重定向网站地址
 
-  LANDING_PRICING_TITLE: '价格表',
-  LANDING_PRICING_P: 'NotionNext开源免费，此处仅演示订阅付费功能！请勿购买！',
+  LANDING_PRICING_TITLE: 'прайс-лист',
+  LANDING_PRICING_P: 'NotionNext является бесплатной программой с открытым исходным кодом, здесь демонстрируется только функция платной подписки! Пожалуйста, не покупайте!',
 
-  LANDING_PRICING_1_TITLE: '个人版',
-  LANDING_PRICING_1_PRICE: '免费',
-  LANDING_PRICING_1_CONTENT: '项目源代码,部署教程,不定时技术答疑',
-  LANDING_PRICING_1_BUTTON: '开始体验',
+  LANDING_PRICING_1_TITLE: 'Материалы, уроки, видео',
+  LANDING_PRICING_1_PRICE: 'бесплатно',
+  LANDING_PRICING_1_CONTENT: 'Исходный код проекта, учебники по развертыванию, время от времени технические вопросы и ответы',
+  LANDING_PRICING_1_BUTTON: 'Начало работы',
   LANDING_PRICING_1_URL: 'https://docs.tangly1024.com/about',
 
-  LANDING_PRICING_2_TITLE: '捐赠版',
-  LANDING_PRICING_2_PRICE: '$9.9/月',
-  LANDING_PRICING_2_CONTENT: '项目源代码,部署教程,长期技术答疑,代码升级合并,内部社群',
-  LANDING_PRICING_2_BUTTON: '立即购买',
+  LANDING_PRICING_2_TITLE: 'Издание для доноров',
+  LANDING_PRICING_2_PRICE: '9.9$',
+  LANDING_PRICING_2_CONTENT: 'Исходный код проекта, учебники по развертыванию, долгосрочные технические вопросы и ответы, обновление и объединение кода, внутреннее сообщество',
+  LANDING_PRICING_2_BUTTON: 'Купить сейчас',
   LANDING_PRICING_2_URL: 'https://tangly1024.lemonsqueezy.com/checkout/buy/0adb9153-0799-4f51-91aa-1f06391ea4e0',
 
-  LANDING_PRICING_3_TITLE: '企业版',
-  LANDING_PRICING_3_PRICE: '$59/月',
-  LANDING_PRICING_3_CONTENT: '项目源代码,部署教程,VIP技术咨询,代码升级合并,内部社群,定制功能开发,SEO优化',
-  LANDING_PRICING_3_BUTTON: '立即购买',
+  LANDING_PRICING_3_TITLE: 'Enterprise Edition',
+  LANDING_PRICING_3_PRICE: '59$',
+  LANDING_PRICING_3_CONTENT: 'Исходный код проекта, учебники по внедрению, VIP-технический консалтинг, объединение для обновления кода, внутреннее сообщество, разработка пользовательских функций, SEO-оптимизация',
+  LANDING_PRICING_3_BUTTON: 'Купить сейчас',
   LANDING_PRICING_3_URL: 'https://tangly1024.lemonsqueezy.com/checkout/buy/df924d66-09dc-42a4-a632-a6b0c5cc4f28',
 
-  LANDING_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_LANDING_NEWSLETTER || false // 是否开启邮件订阅 请先配置mailchimp功能 https://docs.tangly1024.com/article/notion-next-mailchimp
+  LANDING_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_LANDING_NEWSLETTER || false // Нужно ли включать подписку на почту Пожалуйста, сначала настройте функцию mailchimp https://docs.tangly1024.com/article/notion-next-mailchimp
 }
 export default CONFIG
