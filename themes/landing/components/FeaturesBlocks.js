b0242ddcac859ca8f1e6e9c104c5aec60e387444
igotocrypto <pakiboy@yandex.ru>
2024-03-22 14:26:19 +0300

    landing feature block

--- themes/landing/components/FeaturesBlocks.js
+++ themes/landing/components/FeaturesBlocks.js
@@ -23,20 +23,23 @@ export default function FeaturesBlocks() {
 
               {/* 1st item */}
               <div className="relative flex flex-col items-center p-6 bg-white rounded-md shadow-xl border">
-                <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                  <g fill="none" fillRule="evenodd">
-                    <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
-                    <g strokeWidth="2">
-                      <path className="stroke-current text-blue-300" d="M34.514 35.429l2.057 2.285h8M20.571 26.286h5.715l2.057 2.285" />
-                      <path className="stroke-current text-white" d="M20.571 37.714h5.715L36.57 26.286h8" />
-                      <path className="stroke-current text-blue-300" strokeLinecap="square" d="M41.143 34.286l3.428 3.428-3.428 3.429" />
-                      <path className="stroke-current text-white" strokeLinecap="square" d="M41.143 29.714l3.428-3.428-3.428-3.429" />
+                <a href="https://www.youtube.com/@igotocrypto" target="_blank" rel="noopener noreferrer">
+                  <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="none" fillRule="evenodd">
+                      <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
+                      <g strokeWidth="2">
+                        <path className="stroke-current text-blue-300" d="M34.514 35.429l2.057 2.285h8M20.571 26.286h5.715l2.057 2.285" />
+                        <path className="stroke-current text-white" d="M20.571 37.714h5.715L36.57 26.286h8" />
+                        <path className="stroke-current text-blue-300" strokeLinecap="square" d="M41.143 34.286l3.428 3.428-3.428 3.429" />
+                        <path className="stroke-current text-white" strokeLinecap="square" d="M41.143 29.714l3.428-3.428-3.428-3.429" />
+                      </g>
                     </g>
-                  </g>
-                </svg>
+                  </svg>
+                </a>
                 <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">{siteConfig('LANDING_FEATURES_BLOCK_1_TITLE', null, CONFIG)}</h4>
                 <p className="text-gray-600 text-center">{siteConfig('LANDING_FEATURES_BLOCK_1_P', null, CONFIG)}</p>
               </div>
+
 
               {/* 2nd item */}
               <div className="relative flex flex-col items-center p-6 bg-white rounded-md shadow-xl border">
