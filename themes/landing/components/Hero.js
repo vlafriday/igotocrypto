bc0b06ab6b240da93e12a3c25cd3cf545a3fa2ab
vpyatnitsa <pakiboy@yandex.ru>
2024-03-24 08:05:37 +0300

    landing

--- themes/landing/components/Hero.js
+++ themes/landing/components/Hero.js
@@ -1,11 +1,21 @@
-import CONFIG from '../config'
-import ModalVideo from './ModalVideo'
-import { siteConfig } from '@/lib/config'
+import React, { useState } from 'react';
+import CONFIG from '../config';
+import { siteConfig } from '@/lib/config';
+import ModalVideo from './ModalVideo';
 
 export default function Hero() {
-  return (
-        <section className="relative">
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <section className="relative">
             {/* Illustration behind hero content */}
             <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
                 <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
@@ -24,10 +34,8 @@ export default function Hero() {
             </div>
 
             <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
                 {/* Hero content */}
                 <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
                     {/* Section header */}
                     <div className="text-center pb-12 md:pb-16">
                         <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
@@ -37,37 +45,40 @@ export default function Hero() {
                             <p className="text-xl text-gray-600 dark:text-gray-400 mb-8" data-aos="zoom-y-out" data-aos-delay="150">{siteConfig('LANDING_HERO_P_1', null, CONFIG)}</p>
                             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                                 <div>
-                                    <a target='_self' className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0"
-                                        href={siteConfig('LANDING_HERO_BUTTON_1_LINK', null, CONFIG)} rel="noreferrer">
+                                    <a target='_self' className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href={siteConfig('LANDING_HERO_BUTTON_1_LINK', null, CONFIG)} rel="noreferrer">
                                         {siteConfig('LANDING_HERO_BUTTON_1_TEXT', null, CONFIG)}
                                     </a>
                                 </div>
                                 <div>
-                                    <a target='_self' className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4"
-                                        href={siteConfig('LANDING_HERO_BUTTON_2_LINK', null, CONFIG)} rel="noreferrer">
+                                    <a target='_self' className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href={siteConfig('LANDING_HERO_BUTTON_2_LINK', null, CONFIG)} rel="noreferrer">
                                         {siteConfig('LANDING_HERO_BUTTON_2_TEXT', null, CONFIG)}
                                     </a>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                     {/* Hero image */}
-                    <a href={siteConfig('HERO_VIDEO_URL', null, CONFIG)} target="_blank" rel="noopener noreferrer">
-                      <ModalVideo
-                        thumb={siteConfig('LANDING_HERO_VIDEO_IMAGE', null, CONFIG)}
-                        thumbWidth={768}
-                        thumbHeight={432}
-                        thumbAlt={siteConfig('HERO_HEADER_1', null, CONFIG)}
-                        video={siteConfig('HERO_VIDEO_URL', null, CONFIG)}
-                        videoWidth={1920}
-                        videoHeight={1080}
-                      />
-                    </a>
-
+                    <img
+                        src={siteConfig('LANDING_HERO_VIDEO_IMAGE', null, CONFIG)}
+                        alt={siteConfig('HERO_HEADER_1', null, CONFIG)}
+                        onClick={openModal}
+                        className="cursor-pointer"
+                    />
+                    {/* Modal Video */}
+                    {isModalOpen && (
+                        <ModalVideo
+                            video={siteConfig('HERO_VIDEO_URL', null, CONFIG)}
+                            onClose={closeModal}
+                            thumb={siteConfig('LANDING_HERO_VIDEO_IMAGE', null, CONFIG)}
+                            thumbWidth={768}
+                            thumbHeight={432}
+                            thumbAlt={siteConfig('HERO_HEADER_1', null, CONFIG)}
+                            videoWidth={1920}
+                            videoHeight={1080}
+                        />
+                    )}
                 </div>
-
             </div>
         </section>
-  )
+    );
 }
