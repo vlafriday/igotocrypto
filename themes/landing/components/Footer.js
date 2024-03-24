61c754854b89af29218e6e6badfac4ccfb316052
vpyatnitsa <pakiboy@yandex.ru>
2024-03-24 10:13:06 +0300

    landing

--- themes/landing/components/Footer.js
+++ themes/landing/components/Footer.js
@@ -56,19 +56,19 @@ export default function Footer() {
                      {/*    <h6 className="text-gray-800 font-medium mb-2">Инвестируй</h6> */}
                         <ul className="text-sm">
                             <li className="mb-2">
-                                 <a className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"> </a>
+                                 <a href="/about?theme=gitbook" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"> </a>
                             </li>
                             <li className="mb-2">
-                                 <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"> </a>
+                                 <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Инвестируй</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"> </a>
+                                {/* <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Github</a> */}
                             </li>
                             <li className="mb-2">
-                                 <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"> </a>
+                                {/* <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Notion</a> */}
                             </li>
                             <li className="mb-2">
-                               <a href="/about?theme=gitbook" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Инвестируй</a>
+                                {/* <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">NextJs</a> */}
                             </li>
                         </ul>
                     </div>
@@ -78,10 +78,10 @@ export default function Footer() {
                      {/*    <h6 className="text-gray-800 font-medium mb-2">Торгуй</h6> */}
                         <ul className="text-sm">
                             <li className="mb-2">
-                                <a href="/payment" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Торгуй</a>
+                                <a  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"> </a>
                             </li>
                             <li className="mb-2">
-                              {/*  <a href="https://docs.tangly1024.com" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Учебное пособие</a> */}
+                                <a href="/payment" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Торгуй</a>
                             </li>
                             <li className="mb-2">
                               {/*  <a href="https://blog.tangly1024.com" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Блог</a> */}
@@ -100,10 +100,10 @@ export default function Footer() {
                         {/* <h6 className="text-gray-800 font-medium mb-2">Зарабатывай</h6> */}
                         <ul className="text-sm">
                             <li className="mb-2">
-                               <a href="/vip" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Зарабатывай</a>
+                               <a  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"> </a>
                             </li>
                             <li className="mb-2">
-                               {/* <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">О нас</a> */}
+                               <a href="/vip" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Зарабатывай</a>
                             </li>
                             <li className="mb-2">
                                {/* <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Ценности компании</a> */}
