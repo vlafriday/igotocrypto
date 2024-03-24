b7acf12947611b8c812d9ee82b808916ea1f1df8
vpyatnitsa <pakiboy@yandex.ru>
2024-03-24 09:32:20 +0300

    landing

--- themes/landing/components/Footer.js
+++ themes/landing/components/Footer.js
@@ -53,7 +53,7 @@ export default function Footer() {
                     {/* 2nd block */}
                     <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
                         {/* <p className="text-gray-800 font-medium mb-2"><a href="#0" target="_blank" rel="noopener noreferrer">Зарабатывай</a></p> */}
-                        <h6 className="text-gray-800 font-medium mb-2">Инвестируй</h6>
+                        <span className="text-gray-800 font-medium mb-2">Инвестируй</span>
                         <ul className="text-sm">
                             <li className="mb-2">
                                 {/* <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Отказ от ответственности</a> */}
