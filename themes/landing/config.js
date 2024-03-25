0a3c9d79e8b749112d45645f0c9e764a5be5d342
vpyatnitsa <pakiboy@yandex.ru>
2024-03-25 23:47:35 +0300

    landing

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -13,8 +13,8 @@ const CONFIG = {
   LANDING_HERO_BUTTON_1_LINK: '#features',
   LANDING_HERO_BUTTON_2_TEXT: 'Пройти путь',
   LANDING_HERO_BUTTON_2_LINK: '#pricing',
-  LANDING_HERO_VIDEO_IMAGE: 'https://i.ytimg.com/vi/jwRgxGL1gic/maxresdefault.jpg',
-     HERO_VIDEO_URL: 'https://www.youtube.com/watch?v=D8aGQW7ynsg',
+  LANDING_HERO_VIDEO_IMAGE: 'https://i.ytimg.com/vi/LTvCJ5UG4pU/maxresdefault.jpg',
+     HERO_VIDEO_URL: 'https://youtu.be/LTvCJ5UG4pU',
   LANDING_HERO_VIDEO_IFRAME: 'https://www.youtube.com/embed/D8aGQW7ynsg?si=yo0oRgJ9v8cn-GcA',
   LANDING_HERO_VIDEO_TIPS: 'Смотреть полное видео',
 
