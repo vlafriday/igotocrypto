4fa75ecbf766354fcb71094433d9d221f1c679c3
vpyatnitsa <pakiboy@yandex.ru>
2024-03-25 23:51:24 +0300

    landing

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -14,7 +14,7 @@ const CONFIG = {
   LANDING_HERO_BUTTON_2_TEXT: 'Пройти путь',
   LANDING_HERO_BUTTON_2_LINK: '#pricing',
   LANDING_HERO_VIDEO_IMAGE: 'https://i.ytimg.com/vi/LTvCJ5UG4pU/maxresdefault.jpg',
-     HERO_VIDEO_URL: 'https://youtu.be/LTvCJ5UG4pU',
+     // HERO_VIDEO_URL: 'https://youtu.be/LTvCJ5UG4pU',
   LANDING_HERO_VIDEO_IFRAME: 'https://www.youtube.com/embed/LTvCJ5UG4pU',
   LANDING_HERO_VIDEO_TIPS: 'Смотреть полное видео',
 
