e2dc92f183bb74af1cc432d5f2a527a1ee3b0ab1
vpyatnitsa <pakiboy@yandex.ru>
2024-03-27 06:38:45 +0300

    landing

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   LANDING_HERO_BUTTON_1_LINK: '#features',
   LANDING_HERO_BUTTON_2_TEXT: 'Пройти путь',
   LANDING_HERO_BUTTON_2_LINK: '#pricing',
-  LANDING_HERO_VIDEO_IMAGE: 'https://mnbc.info/wp-content/uploads/2024/02/notcoin-telegram-obzor.png',
+  LANDING_HERO_VIDEO_IMAGE: 'https://i.ytimg.com/vi/-pbs-MHWcTE/maxresdefault.jpg',
      // HERO_VIDEO_URL: 'https://youtu.be/LTvCJ5UG4pU',
   LANDING_HERO_VIDEO_IFRAME: 'https://www.youtube.com/embed/-pbs-MHWcTE',
   LANDING_HERO_VIDEO_TIPS: 'Смотреть полное видео',
